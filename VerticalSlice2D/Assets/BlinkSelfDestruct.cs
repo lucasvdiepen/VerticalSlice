f5daa916f0611e475c415d58116f105dcca8768f
PiterGroot <70685433+PiterGroot@users.noreply.github.com>
2021-12-01 12:11:45 +0100

    Fixed curly bracket

--- VerticalSlice2D/Assets/BlinkSelfDestruct.cs
+++ VerticalSlice2D/Assets/BlinkSelfDestruct.cs
@@ -6,7 +6,8 @@ public class BlinkSelfDestruct : MonoBehaviour
 {
     //this function is called as a animation event so we know when the blinking
     //combatslider should be destroyed
-    public void SelfDestruct() {
+    public void SelfDestruct() 
+    {
         Destroy(gameObject);
     }
 }
