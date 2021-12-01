77ee99e20bf6baf20c11a3e3dbb3c44fbef34111
Bart de Boer <bartdb2001@gmail.com>
2021-12-01 15:14:00 +0100

    TPTEXT

--- VerticalSlice2D/Assets/Scripts/TPBar.cs
+++ VerticalSlice2D/Assets/Scripts/TPBar.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class TPBar : MonoBehaviour
 {
-    public float startingY;
+    private float startingY;
+    [SerializeField] private TextMeshProUGUI TPText;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,19 +17,37 @@ public class TPBar : MonoBehaviour
 
     void UpdateSprite()
     {
-        Transform spriteMask = GameObject.Find("TPBar_Spritemask").GetComponent<Transform>();
         GameObject thePlayer = GameObject.Find("Player");
         Player playerTP = thePlayer.GetComponent<Player>();
+        Transform spriteMask = GameObject.Find("TPBar_Spritemask").GetComponent<Transform>();
 
         //this is very precise and hopefully still works after moving it please dont kill me
         spriteMask.position = new Vector2(spriteMask.position.x, startingY+(0.02805f*playerTP.TP));
         //0.02805f is de sprite hoogte. Zodra wij sprites krijgen MOET DIT AANGEPAST WORDEN OM GOED TE WERKEN. Ik zou er een functie voor maken maar ik gebruik hier geen spriterenderer, dus kan ik niet de grootte aanroepen.
     }
 
+    void UpdateText()
+    {
+        
+        GameObject thePlayer = GameObject.Find("Player");
+        Player player = thePlayer.GetComponent<Player>();
+        
+        if (player.TP < 100)
+        {
+            TPText.text = "<i>T\nP\n</i>" + player.TP.ToString() + "\n%";
+        }
+       
+        if (player.TP >= 100)
+        {
+            TPText.text = "<i>T\nP\n</i>" + "<color=yellow>M\n A\n  X</color>";
+        }
+    }
+
     // Update is called once per frame
     void Update()
     {
-        UpdateSprite();   
+        UpdateSprite();
+        UpdateText();
     }
 
     public void AddTP(float functionTP)
