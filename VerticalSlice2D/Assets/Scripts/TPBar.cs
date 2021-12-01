2e7dd998789109bf9c2db07fe731bac0602eecbd
Bart de Boer <bartdb2001@gmail.com>
2021-12-01 12:43:16 +0100

    extra comments en placeholders

--- VerticalSlice2D/Assets/Scripts/TPBar.cs
+++ VerticalSlice2D/Assets/Scripts/TPBar.cs
@@ -4,21 +4,23 @@ using UnityEngine;
 
 public class TPBar : MonoBehaviour
 {
+    public float startingY;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Transform spriteMask = GameObject.Find("TPBar_Spritemask").GetComponent<Transform>();
+        startingY = spriteMask.position.y;
     }
 
     void UpdateSprite()
     {
-        //y = 0.715
-        //y = 3.52
         Transform spriteMask = GameObject.Find("TPBar_Spritemask").GetComponent<Transform>();
         GameObject thePlayer = GameObject.Find("Player");
         Player playerTP = thePlayer.GetComponent<Player>();
-        spriteMask.position = new Vector2(spriteMask.position.x, 0.16f+(0.02805f*playerTP.TP));
 
+        //this is very precise and hopefully still works after moving it please dont kill me
+        spriteMask.position = new Vector2(spriteMask.position.x, startingY+(0.02805f*playerTP.TP));
+        //0.02805f is de sprite hoogte. Zodra wij sprites krijgen MOET DIT AANGEPAST WORDEN OM GOED TE WERKEN. Ik zou er een functie voor maken maar ik gebruik hier geen spriterenderer, dus kan ik het niet oproepen.
     }
 
     // Update is called once per frame
