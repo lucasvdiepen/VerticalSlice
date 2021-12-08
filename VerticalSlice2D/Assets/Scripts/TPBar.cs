7e08653e66e360372f73cef55abbeacb292a0bc8
Bart de Boer <bartdb2001@gmail.com>
2021-12-08 15:13:57 +0100

    script fixes NAV Lucas' feedback

--- VerticalSlice2D/Assets/Scripts/TPBar.cs
+++ VerticalSlice2D/Assets/Scripts/TPBar.cs
@@ -5,46 +5,49 @@ using TMPro;
 
 public class TPBar : MonoBehaviour
 {
+    public float TP = 0;
     private float startingY;
     [SerializeField] private TextMeshProUGUI tpText;
     [SerializeField] private GameObject orangeTPBar;
     [SerializeField] private GameObject max;
     [SerializeField] private GameObject tpPercentage;
+    [SerializeField] Transform spriteMask;
 
     // Start is called before the first frame update
     void Start()
     {
-        Transform spriteMask = GameObject.Find("TPBar_Spritemask").GetComponent<Transform>();
+        
         startingY = spriteMask.position.y;
     }
 
     void UpdateSprite()
     {
-        GameObject thePlayer = GameObject.Find("Player");
-        Player playerTP = thePlayer.GetComponent<Player>();
         Transform spriteMask = GameObject.Find("TPBar_Spritemask").GetComponent<Transform>();
 
         //this is very precise and hopefully still works after moving it please dont kill me
-        spriteMask.position = new Vector2(spriteMask.position.x, startingY+(0.02805f*playerTP.TP));
+        spriteMask.position = new Vector2(spriteMask.position.x, startingY+(0.02805f*TP));
         //0.02805f is de sprite hoogte. Zodra wij sprites krijgen MOET DIT AANGEPAST WORDEN OM GOED TE WERKEN. Ik zou er een functie voor maken maar ik gebruik hier geen spriterenderer, dus kan ik niet de grootte aanroepen.
     }
 
-    void UpdateText()
+    void UpdateTextAndTP()
     {
-        
-        GameObject thePlayer = GameObject.Find("Player");
-        Player player = thePlayer.GetComponent<Player>();
-        
-        if (player.TP < 100)
+
+        if (TP < 0)
         {
-            tpText.text = player.TP.ToString();
+            TP = 0;
+        }
+        
+        if (TP < 100)
+        {
+            tpText.text = TP.ToString();
             orangeTPBar.SetActive(true);
             max.SetActive(false);
             tpPercentage.SetActive(true);
         }
        
-        if (player.TP >= 100)
+        if (TP >= 100)
         {
+            TP = 100;
             tpText.text = "";
             orangeTPBar.SetActive(false);
             max.SetActive(true);
@@ -56,14 +59,11 @@ public class TPBar : MonoBehaviour
     void Update()
     {
         UpdateSprite();
-        UpdateText();
+        UpdateTextAndTP();
     }
 
     public void AddTP(float functionTP)
     {
-        GameObject thePlayer = GameObject.Find("Player");
-        Player playerTP = thePlayer.GetComponent<Player>();
-
-        playerTP.TP = playerTP.TP + functionTP;
+        TP = TP + functionTP;
     }
 }
