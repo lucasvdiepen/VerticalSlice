58d5a6bda27ea287b2b55ce968b9f61879739481
lucasvdiepen <49072755+lucasvdiepen@users.noreply.github.com>
2021-12-09 17:48:32 +0100

    Added abstract menu class

--- VerticalSlice2D/Assets/Scripts/Selection/ActSelectionLogic.cs
+++ VerticalSlice2D/Assets/Scripts/Selection/ActSelectionLogic.cs
@@ -2,32 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ActSelectionLogic : MonoBehaviour
+public class ActSelectionLogic : Menu
 {
-    public GameObject[] heartIcons;
-    public GameObject menuHolder;
-    private int selectedButton = 0;
-    private bool canInteract = false;
-
-    public void OpenMenu()
+    public override void RunInput()
     {
-        menuHolder.SetActive(true);
-        canInteract = true;
-    }
+        base.RunInput();
 
-    public void CloseMenu()
-    {
-        menuHolder.SetActive(false);
-        canInteract = false;
-    }
-
-    private void Update()
-    {
-        RunInput();
-    }
-
-    private void RunInput()
-    {
         if(canInteract)
         {
             if (Input.GetKeyDown(KeyCode.LeftArrow))
@@ -41,35 +21,12 @@ public class ActSelectionLogic : MonoBehaviour
                 if (selectedButton + 2 < heartIcons.Length) selectedButton += 2;
                 UpdateButtonSprite();
             }
-
-            if (Input.GetKeyDown(KeyCode.UpArrow))
-            {
-                if (selectedButton - 1 >= 0) selectedButton -= 1;
-                UpdateButtonSprite();
-            }
-
-            if (Input.GetKeyDown(KeyCode.DownArrow))
-            {
-                if (selectedButton + 1 < heartIcons.Length) selectedButton += 1;
-                UpdateButtonSprite();
-            }
-
-            if (Input.GetKeyDown(KeyCode.Space)) ExecuteButton();
         }
     }
 
-    public void UpdateButtonSprite()
+    public override void ExecuteButton()
     {
-        for(int i = 0; i < heartIcons.Length; i++)
-        {
-            if (i == selectedButton) heartIcons[i].SetActive(true);
-            else heartIcons[i].SetActive(false);
-        }
-    }
-
-    public void ExecuteButton()
-    {
-        switch(selectedButton)
+        switch (selectedButton)
         {
             case 3:
                 CloseMenu();
