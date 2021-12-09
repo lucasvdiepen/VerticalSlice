0cc56cdefa46ba85ce780a0bc351746ae6f43da2
lucasvdiepen <49072755+lucasvdiepen@users.noreply.github.com>
2021-12-09 16:47:24 +0100

    Added working act selection menu

--- VerticalSlice2D/Assets/Scripts/Selection/ActSelectionLogic.cs
+++ VerticalSlice2D/Assets/Scripts/Selection/ActSelectionLogic.cs
@@ -5,7 +5,21 @@ using UnityEngine;
 public class ActSelectionLogic : MonoBehaviour
 {
     public GameObject[] heartIcons;
+    public GameObject menuHolder;
     private int selectedButton = 0;
+    private bool canInteract = false;
+
+    public void OpenMenu()
+    {
+        menuHolder.SetActive(true);
+        canInteract = true;
+    }
+
+    public void CloseMenu()
+    {
+        menuHolder.SetActive(false);
+        canInteract = false;
+    }
 
     private void Update()
     {
@@ -14,28 +28,33 @@ public class ActSelectionLogic : MonoBehaviour
 
     private void RunInput()
     {
-        if(Input.GetKeyDown(KeyCode.LeftArrow))
+        if(canInteract)
         {
-            if (selectedButton - 2 >= 0) selectedButton -= 2;
-            UpdateButtonSprite();
-        }
+            if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                if (selectedButton - 2 >= 0) selectedButton -= 2;
+                UpdateButtonSprite();
+            }
 
-        if (Input.GetKeyDown(KeyCode.RightArrow))
-        {
-            if (selectedButton + 2 < heartIcons.Length) selectedButton += 2;
-            UpdateButtonSprite();
-        }
+            if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                if (selectedButton + 2 < heartIcons.Length) selectedButton += 2;
+                UpdateButtonSprite();
+            }
 
-        if (Input.GetKeyDown(KeyCode.UpArrow))
-        {
-            if (selectedButton - 1 >= 0) selectedButton -= 1;
-            UpdateButtonSprite();
-        }
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                if (selectedButton - 1 >= 0) selectedButton -= 1;
+                UpdateButtonSprite();
+            }
 
-        if(Input.GetKeyDown(KeyCode.DownArrow))
-        {
-            if (selectedButton + 1 < heartIcons.Length) selectedButton += 1;
-            UpdateButtonSprite();
+            if (Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                if (selectedButton + 1 < heartIcons.Length) selectedButton += 1;
+                UpdateButtonSprite();
+            }
+
+            if (Input.GetKeyDown(KeyCode.Space)) ExecuteButton();
         }
     }
 
@@ -45,6 +64,17 @@ public class ActSelectionLogic : MonoBehaviour
         {
             if (i == selectedButton) heartIcons[i].SetActive(true);
             else heartIcons[i].SetActive(false);
+        }
+    }
+
+    public void ExecuteButton()
+    {
+        switch(selectedButton)
+        {
+            case 3:
+                CloseMenu();
+                FindObjectOfType<ActionSaveManager>().ChangeCurrentPlayerAction(ActionSaveManager.ActionType.SoftVoice);
+                break;
         }
     }
 }
