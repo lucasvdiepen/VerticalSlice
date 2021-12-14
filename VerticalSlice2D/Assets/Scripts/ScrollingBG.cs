b5d49723b5259b16f19009112956e94744f49d0c
PiterGroot <70685433+PiterGroot@users.noreply.github.com>
2021-12-14 12:08:17 +0100

    Added open / close animations for scrolling grid background

--- VerticalSlice2D/Assets/Scripts/ScrollingBG.cs
+++ VerticalSlice2D/Assets/Scripts/ScrollingBG.cs
@@ -6,13 +6,29 @@ public class ScrollingBG : MonoBehaviour
 {
     [SerializeField] float moveSpeed;
     private Renderer _renderer;
+    private Animator bgAnim;
 
-    private void Awake() {
+    private void Awake() 
+    {
+        bgAnim = gameObject.GetComponent<Animator>();
         _renderer = gameObject.GetComponent<Renderer>();
     }
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
         _renderer.material.mainTextureOffset += new Vector2(moveSpeed * Time.deltaTime, -moveSpeed * Time.deltaTime);
+    }
+    public void ToggleGrid(bool OpenClose)
+    {
+        if (OpenClose && bgAnim != null) 
+        {
+            //open
+            bgAnim.SetTrigger("Close");
+        }
+        else 
+        {
+            //close
+            bgAnim.SetTrigger("Open");
+        }
     }
 }
