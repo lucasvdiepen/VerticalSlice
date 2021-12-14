27141cb9cad0237ba829864bb070afebebb776b9
PiterGroot <70685433+PiterGroot@users.noreply.github.com>
2021-12-14 12:27:24 +0100

    EnemySelectie afgemaakt

--- VerticalSlice2D/Assets/Scripts/Selection/EnemySelectionLogic.cs
+++ VerticalSlice2D/Assets/Scripts/Selection/EnemySelectionLogic.cs
@@ -6,71 +6,101 @@ using UnityEngine.Events;
 
 public class EnemySelectionLogic : MonoBehaviour
 {
-    private GameObject selectedEnemy;
+    private bool canSelect;
     private AudioManager audioManager;
-    [SerializeField] private GameObject thisCharacter;
     [SerializeField] private GameObject[] enemies;
     [SerializeField] private Color unselected, selected;
     [SerializeField] private int direction;
     [SerializeField] private List<GameObject> buttons = new List<GameObject>();
     [SerializeField] private Image currentSprite;
+    public GameObject thisPerson;
+    public GameObject selectedEnemy;
 
 
-    private void Awake() {
+    private void Awake() 
+    {
         audioManager = FindObjectOfType<AudioManager>();
     }
 
-    private void Start() {
+    private void Start() 
+    {
         UpdateDirection();
     }
     // Update is called once per frame
-    private void Update() {
-        if (Input.GetKeyDown(KeyCode.RightArrow)) {
-            //move right
-            direction++;
-            if (direction == buttons.Count) {
-                direction = 0;
+    private void Update() 
+    {
+        if (canSelect) 
+        {
+            if (Input.GetKeyDown(KeyCode.RightArrow)) 
+            {
+                //move right
+                direction++;
+                if (direction == buttons.Count) 
+                {
+                    direction = 0;
+                }
+                UpdateDirection();
             }
-            UpdateDirection();
-        }
-        if (Input.GetKeyDown(KeyCode.LeftArrow)) {
-            //move left
-            direction--;
-            if (direction == -1) {
-                direction = buttons.Count - 1;
+            if (Input.GetKeyDown(KeyCode.LeftArrow)) 
+            {
+                //move left
+                direction--;
+                if (direction == -1) 
+                {
+                    direction = buttons.Count - 1;
+                }
+                UpdateDirection();
             }
-            UpdateDirection();
-        }
-        if (Input.GetKeyDown(KeyCode.Space)) {
-            ExecuteButton();
+            if (Input.GetKeyDown(KeyCode.Space)) 
+            {
+                ExecuteButton();
+            }
         }
     }
-    private void UpdateDirection() {
+    private void UpdateDirection() 
+    {
         //audioManager.Play("Blip");
         currentSprite = buttons[direction].GetComponent<Image>();
         SetSelectedButton();
     }
-    private void SetSelectedButton() {
-        foreach (GameObject button in buttons) {
+    private void SetSelectedButton() 
+    {
+        foreach (GameObject button in buttons) 
+        {
             button.GetComponent<Image>().color = selected;
         }
         currentSprite.color = unselected;
     }
-    private void ExecuteButton() {
-        if(currentSprite == buttons[1].GetComponent<Image>()) {
+    private void ExecuteButton() 
+    {
+        if(currentSprite == buttons[1].GetComponent<Image>()) 
+        {
             //first enemy
             selectedEnemy = enemies[1];
         }
-        else if(currentSprite == buttons[0].GetComponent<Image>()){
+        else if(currentSprite == buttons[0].GetComponent<Image>())
+        {
             //seceond enemy
             selectedEnemy = enemies[0];
         }
-        transform.parent.gameObject.SetActive(false);
+        CloseMenu();
     }
-    public GameObject GetSelectedEnemy() {
+    public void OpenMenu() 
+    {
+        canSelect = true;
+        transform.GetChild(0).gameObject.SetActive(true);
+    }
+    public void CloseMenu() 
+    {
+        canSelect = false;
+        transform.GetChild(0).gameObject.SetActive(false);
+    }
+    public GameObject GetSelectedEnemy() 
+    {
         return selectedEnemy;
     }
-    public GameObject GetPerson() {
-        return thisCharacter;
+    public GameObject GetPerson() 
+    {
+        return thisPerson;
     }
 }
