a6b03d049934ba10ab73fdc0204f758f8fc113ef
PiterGroot <70685433+PiterGroot@users.noreply.github.com>
2021-12-16 10:33:15 +0100

    Damage text readsety

--- VerticalSlice2D/Assets/Scripts/DamageText/DamageText.cs
+++ VerticalSlice2D/Assets/Scripts/DamageText/DamageText.cs
@@ -2,25 +2,57 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class DamageText : MonoBehaviour
 {
     [SerializeField] private Canvas canvasToSpawn;
     [SerializeField] private GameObject textHolder;
     [SerializeField] private TextMeshProUGUI damageTextPrefab;
-    public void CreateDamageText(float damage, Vector3 spawnPos, Color textColor) {
-       GameObject textHolder_ = Instantiate(textHolder, transform.position, Quaternion.identity);
-        TextMeshProUGUI displayText = Instantiate(damageTextPrefab, spawnPos, Quaternion.identity);
+
+    //example use case:
+    //CreateDamageText(120, placeToSpawn.anchoredPosition, GetCorrectColor("ralsei"), 52);
+    public void CreateDamageText(float damage, Vector3 rectTransformSpawnPos, Color textColor, float fontSize = 52) 
+    {
+        //offset for animation
+        rectTransformSpawnPos += new Vector3(80, 0, 0);
+        //empty gameObject for animation
+        GameObject textHolder_ = Instantiate(textHolder, transform.position, Quaternion.identity);
+        TextMeshProUGUI displayText = Instantiate(damageTextPrefab, rectTransformSpawnPos, Quaternion.identity);
+        //setting the text component parent to the empty gameobject and to the canvas
         displayText.transform.SetParent(textHolder_.transform);
         textHolder_.transform.SetParent(canvasToSpawn.transform);
-        displayText.GetComponent<RectTransform>().anchoredPosition = spawnPos;
-        textHolder_.GetComponent<RectTransform>().anchoredPosition = spawnPos;
+        //setting the correct position
+        displayText.GetComponent<RectTransform>().anchoredPosition = rectTransformSpawnPos;
+        textHolder_.GetComponent<RectTransform>().anchoredPosition = rectTransformSpawnPos;
+        //using the damage text settings
         displayText.color = textColor;
+        displayText.fontSize = fontSize;
         displayText.text = damage.ToString();
     }
-    private void Update() {
-        if (Input.GetKeyDown(KeyCode.Space)) {
-            CreateDamageText(120, new Vector3(0, 0, 0), Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f));
+
+    public Color GetCorrectColor(string character) 
+    {
+        //susie 255, 0, 223, 255
+        //kris 44, 198, 225, 255
+        //ralsei 21, 255, 0, 255
+        Color result = Color.white;
+        switch (character) 
+        {
+            case "kris":
+                result = new Color32(44, 198, 225, 255);
+                break;
+            case "susie":
+                result = new Color32(255, 0, 223, 255);
+                break;
+            case "ralsei":
+                result = new Color32(21, 255, 0, 255);
+                break;
+            default:
+                Debug.LogError("Please only use color characte: 'kris', 'susie or 'ralsei'", this);
+                result = Color.black;
+                break;
         }
+        return result;
     }
 }
